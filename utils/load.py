746e4895abd1476e963e97637fad400cf642377b
Pierre Glandon <pglandonfr@gmail.com>
2023-04-10 23:02:22 +0200

    type: add load_my_data typing

--- utils/load.py
+++ utils/load.py
@@ -1,8 +1,11 @@
 import pickle
 from pathlib import Path
+from typing import Tuple, List
+
+from networkx import Graph
 
 
-def load_my_data(data_dir=Path("data")):
+def load_my_data(data_dir=Path("data")) -> Tuple[List[Graph], List[str], List[Graph]]:
     """Load dataset"""
     with open(data_dir / "training_data.pkl", 'rb') as f:
         train_data = pickle.load(f)
